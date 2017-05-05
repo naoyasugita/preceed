98701f4ed99c62b9db7fcd30331d1a94dc7727ac
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-05-05 11:50:32 +0900

    fix

---
+++ platforms/ios/www/js/local_test.js
@@ -0,0 +1,36 @@
+$(loaded);
+
+function loaded() {
+    showText();
+
+    $("#formButton").click(
+        function() {
+            saveText();
+            showText();
+        });
+}
+
+function saveText() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var datalist = {
+        name: name,
+        email: email,
+        message: message
+    }
+    localStorage.setItem("datalist", JSON.stringify(datalist));
+    JSON.parse(localStorage.getItem("datalist"));
+}
+
+function showText() {
+    var list = $("#list");
+    list.children().remove();
+    var key, value, html = [];
+    for (var i = 0, len = localStorage.length; i < len; i++) {
+        key = localStorage.key(i);
+        value = localStorage.getItem(key);
+        html.push($("<p>").text(value));
+    }
+    list.append(html);
+}
