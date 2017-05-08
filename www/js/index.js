75cf9a15778e19d684bc1c49b46e01a4b8f74216
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-05-09 00:24:24 +0900

    fix index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,45 +3,39 @@ var restext = document.getElementById("restext");
 var savebtn = document.getElementById("save");
 var deletebtn = document.getElementById("delete");
 let arr = [];
+let num = 0;
 if (!window.localStorage) {
     restext.innerHTML = "web Storageに対応しておりません。";
     return;
 }
 savebtn.addEventListener("click", function() {
-    var t = document.getElementById("uname").value;
+    var t = document.getElementById("username").value;
     var n = document.getElementById("email").value;
-    var m = document.getElementById("message").value;
 
-    if ((t == null || t == "") || (n == null || n == "") || (m == null || m == "")) {
-        restext.innerHTML = "データを入力してください";
+    if ((t == null || t == "") || (n == null || n == "")) {
+        check.innerHTML = "データを入力してください";
         return;
     }
+    arr.push({"username":t,"email":n});
 
-    // var arr = '"' + count + '":{"uname":' + t + "," + '"email":' + n + "," +'"message":' + m +"},"
-
-    arr.push({"uname":t,"email":n,"message":m});
-    localStorage.setItem('datalist', arr);
-
-    window.localStorage.setItem("datalist", JSON.stringify(arr));
-    restext.innerHTML = "データを保存しました";
+    var con = new Contact("datalist");
+    con.save(arr);
+    console.log(con);
+    check.innerHTML = "データを保存しました<br>";
     display();
 }, true);
-
-
-window.addEventListener("storage", function(evt) {
-  console.log(evt.storageArea);
-}, true);
-
-localStorage.setItem("hoge", "bbb");
-
 function display() {
-       let data = JSON.parse(window.localStorage.getItem("datalist"));
-        for (let i in data){
-            console.log("name;" + data[i].uname);
-            console.log("email:" + data[i].email);
-            console.log("message:" + data[i].message);
-            console.log("-----------------");
-        }
+        var lo = new LocalStorage("datalist");
+        var d = lo.load();
+        console.log(num);
+        console.log(d[num].username);
+        var dl = document.createElement("a");
+        dl.id = "restext";
+        document.getElementById("res").appendChild(dl);
+        var dt = document.createElement("p1");
+        document.getElementById("restext").appendChild(dt);
+        dt.innerHTML = "name:" + d[num].username + "<br>" + "mail:" + d[num].email + "<br><br>";
+        num += 1;
 };
 
 deletebtn.addEventListener("click", function() {
@@ -53,5 +47,3 @@ deletebtn.addEventListener("click", function() {
     restext.innerHTML = "保存データを削除しました。";
 }, true);
 }
-
-
