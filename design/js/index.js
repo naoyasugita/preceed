0049ba1213ee59016ccd29fd49907414ba0e85d5
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-06-06 17:38:53 +0900

    add screen transition

---
+++ design/js/index.js
@@ -0,0 +1,24 @@
+let con
+window.onload = function() {
+    var restext = document.getElementById("restext");
+    var savebtn = document.getElementById("add");
+    con = new Contact("datalist");
+    // var deletebtn = document.getElementById("delete");
+    let arr = [];
+    let num = 0;
+    if (!window.localStorage) {
+        restext.innerHTML = "web Storageに対応しておりません。";
+        return;
+    }
+    savebtn.addEventListener("click", function() {
+        var name = document.getElementById("username").value;
+        var mail = document.getElementById("email").value;
+        if ((name == null || name == "") || (mail == null || mail == "")) {
+            alert("データを入力してください");
+            return;
+        }
+
+        con.add(name, mail);
+        console.log(con);
+    }, true);
+}
