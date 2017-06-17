16b01ab227dbf3cc97f31a722d30e1574ebfeb6a
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-06-17 16:41:56 +0900

    fix 編集button

--- design/js/dataset.js
+++ design/js/dataset.js
@@ -2,10 +2,7 @@ let lo
 window.onload=function(){
     lo = new LocalStorage("datalist");
     var d = lo.load();
-    // let height = 84
-    // let space = 10
     let buttons = document.getElementById('buttons')
-
     for ( var key in d ) {
         buttons.innerHTML +=
             '<div class="waku1">' +
@@ -14,8 +11,8 @@ window.onload=function(){
                          key +
                     '</div>' +
                     '<div class="wakub">' +
-                        '<a href="new.html" >' +
-                        '<button type="submit" class="gradientButton" onclick="edit(' + key + ',' + d[key] + '")>' +
+                        '<a href="edit.html" >' +
+                        '<button type="submit" class="gradientButton" onclick="edit(\'' + key + '\', \'' + d[key] + '\')">' +
                             '編集' +
                         '</button>' +
                         '</a>' +
@@ -23,16 +20,9 @@ window.onload=function(){
                 '</div>' +
             '</div>'
         }
-    console.log('<button type="submit" class="gradientButton" onclick="edit(' + key + ',' + d[key] + ')">' +
-                            '編集');
-
-
-
-    //     buttons.innerHTML += '<div id="back" style="top:' + ((space * count) + (height * (count++))) + 'px"><div><button type="submit" class="edit" onclick="edit(\'' + key + '\', \'' + d[key] + '\')">編集</button>' + key + '</div></div>'
-    // }
 }
 
 function edit( name, email ){
     localStorage.setItem( 'edit', JSON.stringify( { name: name, email: email } ) )
-    window.location.href = 'edit.html'
+    window.location.href = './edit.html'
 }
